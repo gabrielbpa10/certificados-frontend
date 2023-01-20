6efacdfc8dbf7682e627368d08996f4ca956ab6d
Gabriel Araujo <gabrielbpa10@gmail.com>
2023-01-20 16:32:36 -0300

    feat:conectando projeto ao firebase e colocando componente de paginacao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './templates/header/header.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
+import { CertificadosComponent } from './view/certificados/certificados.component';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAnalyticsModule } from '@angular/fire/compat/analytics';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { environment } from 'src/environments/environment.prod';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    CertificadosComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAnalyticsModule,
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,16 @@
 
 export const environment = {
   production: false,
-  urlHome: 'file:///home/gabriel/Documentos/site-pessoal-desenvolvedor/index.html'
+  urlHome: 'file:///home/gabriel/Documentos/site-pessoal-desenvolvedor/index.html',
+  firebase: {
+    apiKey: "AIzaSyC12uOElCqGL0oaMGq0bjTGjt_GkSQ_nmE",
+  authDomain: "certificados-portifolio-fc277.firebaseapp.com",
+  projectId: "certificados-portifolio-fc277",
+  storageBucket: "certificados-portifolio-fc277.appspot.com",
+  messagingSenderId: "581826480966",
+  appId: "1:581826480966:web:ca21775d168dbca449b347",
+  measurementId: "G-BKF0MXWVBN"
+  }
 };
 
 /*
